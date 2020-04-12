dc7c63a4afb6a43252e1d413b2b1d8715a6f7c86
revive one <emyoung611@gmail.com>
2020-04-12 22:22:53 +0900

    I learned authentication system by own. thank you for Kod dev, the youtuber who makes people could develop the chatting app, not cheating app. huh huh huh, loooooooooool

---
+++ app/src/main/java/com/example/quartette/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.quartette;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Quartette");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+}
